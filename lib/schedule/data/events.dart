99fec6429f6a7a9da128c9f5a2c1be84f227bb60
Felix Angelov <felangelov@gmail.com>
2025-08-14 22:24:46 -0500

    feat: update activities for 2025

--- lib/schedule/data/events.dart
+++ lib/schedule/data/events.dart
@@ -4,58 +4,8 @@ final allEvents = <Event>[...day1, ...day2, ...day3];
 
 final day1 = <Event>[
   Activity(
-    name: 'Skansen',
-    startTime: DateTime(2025, 8, 31, 12, 30),
-    duration: const Duration(hours: 2),
-    description: '''
-At Skansen you will get to see Swedish history, culture and wildlife in one captivating setting.''',
-    image: 'assets/activities/zoo.jpg',
-    location: const Location(
-      name: 'Skansenbutiken',
-      coordinates: (59.3244718, 18.100444),
-    ),
-  ),
-  Activity(
-    name: 'Bouldering',
-    startTime: DateTime(2025, 8, 31, 12, 30),
-    duration: const Duration(hours: 2),
-    description: '''
-You'll get to experience the thrill of climbing/bouldering at Klättercentret Telefonplan.''',
-    image: 'assets/activities/bouldering.jpg',
-    location: const Location(
-      name: 'Klättercentret Telefonplan',
-      coordinates: (59.2990912, 17.9915368),
-    ),
-  ),
-  Activity(
-    name: 'Disc Golf',
-    startTime: DateTime(2025, 8, 31, 12, 30),
-    duration: const Duration(hours: 2),
-    description: '''
-A perfect opportunity to connect with nature. Throw frisbees towards targets and enjoy the beautiful outdoors.''',
-    image: 'assets/activities/disc_golf.jpg',
-    location: const Location(
-      name: 'Kaknäs DiscGolfPark',
-      coordinates: (59.3341683, 18.1282904),
-    ),
-  ),
-  Activity(
-    name: 'Gröna Lund',
-    startTime: DateTime(2025, 8, 31, 12, 30),
-    duration: const Duration(hours: 2),
-    description: '''
-Gröna Lund is a classic amusement park filled with thrilling rides, live music, and a vibrant atmosphere right in the heart of Stockholm.
-
-You'll even get to do the classic pentathlon!''',
-    image: 'assets/activities/pentathlon.jpg',
-    location: const Location(
-      name: 'Gröna Lund',
-      coordinates: (59.3233564, 18.0963901),
-    ),
-  ),
-  Activity(
     name: 'Bike Tour',
-    startTime: DateTime(2025, 8, 31, 12, 30),
+    startTime: DateTime(2025, 8, 31, 13),
     duration: const Duration(hours: 2),
     description: '''
 You will get to bike around the beautiful city of Stockholm.
@@ -68,32 +18,34 @@ There will be two routes to pick from depending on how far you want to bike.''',
     ),
   ),
   Activity(
-    name: 'Kayak Tour',
+    name: 'Gröna Lund Theme Park',
     startTime: DateTime(2025, 8, 31, 13),
     duration: const Duration(hours: 2),
     description: '''
-You'll get to kayak between the islands and under the bridges, experiencing Stockholm in a way you've never seen it before.''',
-    image: 'assets/activities/kayak_tour.jpg',
+Gröna Lund is a classic amusement park filled with thrilling rides, live music, and a vibrant atmosphere right in the heart of Stockholm.
+
+You'll even get to do the classic pentathlon!''',
+    image: 'assets/activities/pentathlon.jpg',
     location: const Location(
-      name: 'Kayak centre in Rålis park',
-      coordinates: (59.3254818, 18.0240966),
+      name: 'Gröna Lund',
+      coordinates: (59.3233564, 18.0963901),
     ),
   ),
   Activity(
-    name: 'Hey Sthlm + VREX',
-    startTime: DateTime(2025, 8, 31, 15),
+    name: 'Skansen Zoo & History Village Tour',
+    startTime: DateTime(2025, 8, 31, 13),
     duration: const Duration(hours: 2),
     description: '''
-You'll get to play arcade and VR games in the heart of Stockholm.''',
-    image: 'assets/activities/arcade.jpg',
+At Skansen you will get to see Swedish history, culture and wildlife in one captivating setting.''',
+    image: 'assets/activities/zoo.jpg',
     location: const Location(
-      name: 'VREX City',
-      coordinates: (59.33216, 18.0653693),
+      name: 'Skansenbutiken',
+      coordinates: (59.3244718, 18.100444),
     ),
   ),
   Activity(
     name: 'Escape Room',
-    startTime: DateTime(2025, 8, 31, 15),
+    startTime: DateTime(2025, 8, 31, 13),
     duration: const Duration(hours: 2),
     description: '''
 Get locked in a room together with a small group of people and try to get out in time.
@@ -106,6 +58,54 @@ It is both thrilling and requires you to think on your feet!''',
     ),
   ),
   Activity(
+    name: 'Disc Golf',
+    startTime: DateTime(2025, 8, 31, 13),
+    duration: const Duration(hours: 2),
+    description: '''
+Disc golf with the world famous meme-creator Mangirdas and a perfect opportunity to connect with nature. Throw frisbees towards targets and enjoy the beautiful outdoors.''',
+    image: 'assets/activities/disc_golf.jpg',
+    location: const Location(
+      name: 'Kaknäs DiscGolfPark',
+      coordinates: (59.3341683, 18.1282904),
+    ),
+  ),
+  Activity(
+    name: 'Islands Kayak Tour',
+    startTime: DateTime(2025, 8, 31, 13),
+    duration: const Duration(hours: 2),
+    description: '''
+You'll get to kayak between the islands and under the bridges, experiencing Stockholm in a way you've never seen it before.''',
+    image: 'assets/activities/kayak_tour.jpg',
+    location: const Location(
+      name: 'Eden Kajak',
+      coordinates: (59.3255365, 18.0201859),
+    ),
+  ),
+  Activity(
+    name: 'Bouldering',
+    startTime: DateTime(2025, 8, 31, 13),
+    duration: const Duration(hours: 2),
+    description: '''
+You'll get to experience the thrill of climbing/bouldering at Klättercentret Telefonplan.''',
+    image: 'assets/activities/bouldering.jpg',
+    location: const Location(
+      name: 'Klättercentret Telefonplan',
+      coordinates: (59.2990912, 17.9915368),
+    ),
+  ),
+  Activity(
+    name: 'VR Games & Arcade',
+    startTime: DateTime(2025, 8, 31, 14, 30),
+    duration: const Duration(hours: 2),
+    description: '''
+You'll get to play arcade and VR games in the heart of Stockholm.''',
+    image: 'assets/activities/arcade.jpg',
+    location: const Location(
+      name: 'VREX City',
+      coordinates: (59.33216, 18.0653693),
+    ),
+  ),
+  Activity(
     name: '🍽️ Dinner with Strangers',
     startTime: DateTime(2025, 8, 31, 18),
     duration: const Duration(hours: 2),
@@ -114,9 +114,20 @@ Step out of your comfort zone and into a world of new connections. Dinner with S
     location: const Location(name: 'TBD', coordinates: (59.326038, 17.8172502)),
   ),
   Activity(
-    name: '🎯 Flutter & Fun Pub Night',
-    startTime: DateTime(2025, 8, 31, 19, 30),
-    duration: const Duration(hours: 4),
+    name: "🍽️ Speakers' Dinner",
+    startTime: DateTime(2025, 8, 31, 18, 30),
+    duration: const Duration(hours: 2, minutes: 30),
+    description: '''
+This is a dinner hosted at the Google office for speakers and sponsors that have signed up for it.''',
+    location: const Location(
+      name: 'Google Office',
+      coordinates: (59.3334327, 18.0545033),
+    ),
+  ),
+  Activity(
+    name: '🎯 Flutter & Fun Pub Quiz',
+    startTime: DateTime(2025, 8, 31, 20),
+    duration: const Duration(hours: 3),
     description: '''
 Join fellow Flutter enthusiasts for a night of great conversation, a challenging pub quiz, and beers. It's the perfect opportunity to network, relax, and have some fun!''',
     location: const Location(
