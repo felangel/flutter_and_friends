226ce0dfe11e9bcdcb91a2d447dce83395c1657d
Felix Angelov <felangelov@gmail.com>
2025-08-29 23:38:16 -0500

    fix(friends_badge): remove custom nfc manager interactions

--- lib/friends_badge/widgets/write_to_badge_button.dart
+++ lib/friends_badge/widgets/write_to_badge_button.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_and_friends/friends_badge/friends_badge.dart';
 import 'package:friends_badge/friends_badge.dart';
-import 'package:nfc_manager/nfc_manager.dart';
 
 class WriteToBadgeButton extends StatelessWidget {
   const WriteToBadgeButton(this.badge, {super.key});
@@ -16,20 +15,19 @@ class WriteToBadgeButton extends StatelessWidget {
       tooltip: 'Write to badge',
       onPressed: () async {
         try {
-          if (await NfcManager.instance.isAvailable()) {
-            await NfcManager.instance.stopSession();
-          }
           if (!context.mounted) return;
           await WaitingForNfcTap.showLoading(
             context: context,
             job: badge.image.writeToBadge(kernel: badge.ditherKernel),
           );
-        } on PlatformException {
+        } on PlatformException catch (e) {
           if (!context.mounted) return;
           ScaffoldMessenger.of(context).showSnackBar(
-            const SnackBar(
+            SnackBar(
               content: Text(
-                'Error writing to badge, please restart the app and try again.',
+                '''
+Error writing to badge, please restart the app and try again.
+Error: $e''',
               ),
             ),
           );
